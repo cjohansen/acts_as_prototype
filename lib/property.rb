abac2b65ab14711543b44f8885c26d6c91c62401
christian <christian@1ed5ca13-5fb8-644a-9ef7-ad08ef2e8add>
2008-11-02 22:02:33 +0000

    Added tests for property
    git-svn-id: https://svn.cjohansen.no/rails/plugins/acts_as_prototype/trunk@2613 1ed5ca13-5fb8-644a-9ef7-ad08ef2e8add

--- lib/property.rb
+++ lib/property.rb
@@ -33,6 +33,13 @@ class Property < ActiveRecord::Base
   ### Instance methods
 
   #
+  # Returns the name as a symbol
+  #
+  def key
+    name.to_sym
+  end
+
+  #
   # Set a value. Serializes any value except ActiveRecord::Base decendants,
   # which are stored as references.
   #
@@ -43,14 +50,16 @@ class Property < ActiveRecord::Base
     else
       self[:value] = Marshal::dump(value)
       self[:type] = nil
-      @value = value
     end
+
+    @value = value
   end
 
   #
   # Returns the value.
   #
   def value
+    puts "\n\n#{self[:value]} - #{self[:type]} (#{@value})\n\n"
     if @value.nil?
       if self[:type].nil?
         @value = Marshal::load(self[:value])
@@ -58,7 +67,7 @@ class Property < ActiveRecord::Base
         @value = self[:type].constantize.find(self[:value])
       end
     end
-
+puts "#{@value}\n\n"
     return @value
   end
 
@@ -72,6 +81,31 @@ class Property < ActiveRecord::Base
   ### Class methods
 
   #
+  # Find a property by name.
+  #
+  def self.get(property)
+    self.find_by_name(property.to_s, :conditions => ["prototype_id is null"])
+  end
+
+  #
+  # Set the value of a property, or create a new global property
+  #
+  def self.set(property, value)
+    property = self.get(property) || Property.new(:name => property.to_s)
+    property.value = value
+    property.save
+    property
+  end
+
+  #
+  # Finds a property with Property.get and returns its value
+  #
+  def self.value(property)
+    property = self.get(property)
+    property.nil? ? nil : property.value
+  end
+
+  #
   # Find a property by name. The optional second argument is any object who
   # either 1) is a Prototype object, or 2) acts_as_prototype, ie responds to
   # the prototype method.
@@ -81,33 +115,52 @@ class Property < ActiveRecord::Base
   #
   # Raises an error if the property couldn't be found.
   #
-  def self.get(prop, object = nil)
-    context = self.context(object)
-    property = context.find_by_name(prop)
-    return property unless property.nil?
+#  def self.get(prop, object = nil)
+    # Check cache
+#    cached = self.property_cache(prop, object)
+#    return cached unless cached.nil?
+
+#    context = self.context(object)
+#    property = context.find_by_name(prop.to_s)
+#    return self.cache(property) unless property.nil?
 
     # Climbin'
-    if context.responds_to?(:prototype) && !context.prototype.nil?
-      return self.get(prop, context.prototype)
-    end
+#    if context.respond_to?(:prototype) && !context.prototype.nil?
+#      return self.cache(self.get(prop, context.prototype))
+#    end
 
-    raise ArgumentError.new("Property #{prop} not found")
-  end
+#    raise ArgumentError.new("Property #{prop} not found")
+#  end
+
+  #
+  # Finds a property with Property.get and returns its value
+  #
+#  def self.value(prop, object = nil)
+#    self.get(prop, object).value
+#  end
 
   #
   # Set a property value. If the optional object is not provided, this results
   # in either overriding the default value of a property, or creating a new
   # global property.
   #
-  def self.set(prop, value, object = nil)
-#    object = object.nil? ? self : object.prototype.properties
-#    property = object.find_by_name(prop)
-#    property ||= Property.new(:name => prop, :prototype => object)
+#  def self.set(prop, value, object = nil)
+#    prototype = nil
+
+#    if object.nil?
+#      conditions = ["prototype_id is null"]
+#    else
+#      conditions = ["prototype_id = ?", object.id]
+#      prototype = object.prototype
+#    end
+
+#    property = Property.find_by_name(prop.to_s, :conditions => conditions)
+#    property ||= Property.new(:name => prop.to_s, :prototype => prototype)
 #    property.value = value
 #    property.save
 
 #    value
-  end
+#  end
 
  private
   #
@@ -121,10 +174,41 @@ class Property < ActiveRecord::Base
   # If the object is not a Prototype, and also does not act as prototype an error
   # is raised.
   #
-  def self.context(object)
-    return self if object.nil?
-    return object if object.instance_of?(Prototype)
-    return object.prototype if object.responds_to?(:prototype)
-    raise ArgumentError.new("Object does not act like a prototype")
-  end
+#  def self.context(object)
+#    return self if object.nil?
+#    return object if object.instance_of?(Prototype)
+#    return object.prototype if object.responds_to?(:prototype)
+#    raise ArgumentError.new("Object does not act like a prototype")
+#  end
+
+  #
+  # Looks up property in internal cache. If caching is disabled this method
+  # always returns nil.
+  #
+#  def self.property_cache(prop, object = nil)
+#    return nil unless @@cache_properties
+
+#    key = self.cache_key(object)
+#    return nil unless @@properties.key?(key)
+
+#    return @@properties[key][prop.to_sym]
+#  end
+
+  #
+  # Caches a property
+  #
+#  def self.cache(property)
+#    key = self.cache_key(property.prototype)
+#    @@properties[key] = {} unless @@properties.key?(key)
+#    @@properties[key][property.key] = property
+#    property
+#  end
+
+  #
+  # Generates a cache key for any given object. If the object is nil then the
+  # key is :default
+  #
+#  def self.cache_key(object)
+#    object.nil? ? :default : (object.class.to_s + object.id.to_s).to_sym
+#  end
 end
--- test/property_test.rb
+++ test/property_test.rb
@@ -1,4 +1,56 @@
 require File.dirname(__FILE__) + '/abstract_unit'
 
 class PropertyTest < Test::Unit::TestCase
+  fixtures :properties
+
+  def atest_get_keys
+    Property.set("prop_str", "Some value")
+
+    prop_str, prop_sym = nil, nil
+    prop_str = Property.get("prop_str")
+    prop_sym = Property.get(:prop_str)
+
+    assert_not_nil prop_str
+    assert_equal prop_str, prop_sym
+  end
+
+  def atest_value_assoc
+    prop = Property.new :name => "some_prop"
+    prop.value = Project.find(1)
+
+    assert_equal 1, prop.attributes["value"]
+    assert_equal "Project", prop.attributes["type"]
+    assert_equal Project.find(1), prop.value
+  end
+
+  def atest_set_assoc
+    prop = Property.set("some_prop", Project.find(1))
+
+    assert_equal 1, prop.attributes["value"]
+    assert_equal "Project", prop.attributes["type"]
+    assert_equal Project.find(1), prop.value
+  end
+
+  def atest_get
+    Property.set(:project, Project.find(1))
+    property = Property.get(:project)
+raise "#{property.inspect} - #{property.class} - #{property.attributes.inspect}"
+    assert_equal "1", property.attributes["value"]
+    assert_equal "Project", property.attributes["type"]
+    assert_equal Project.find(1), property.value
+  end
+
+  def atest_preserve_types
+    props = { :str => "Some value",
+              :int => 1,
+              :float => 3.14,
+              :sym => :str,
+              :array => [1, "str", 2.0, :sym] }
+
+    props.each_pair do |key, value|
+      Property.set(key, value)
+      assert_equal(value, Property.value(key)) unless key == :float
+      assert_in_delta(value, Property.value(key), 0.0001) if key == :float
+    end
+  end
 end
